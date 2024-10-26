d81b405d592c1d69cc5cf2aff2b17c4f172f3dad
Trần Thái Hòa <thangbado535@gmail.com>
2024-10-26 11:15:55 +0700

    Add Order and OrderDetail entities

--- Data/Entities/OrderDetail.cs
+++ Data/Entities/OrderDetail.cs
@@ -2,5 +2,11 @@
 {
     public class OrderDetail
     {
+        public int Id { get; set; }
+        public virtual required Order Order { get; set; }
+        public virtual Product? Product { get; set; }
+        public int Quantity { get; set; }
+        public double SellingPrice { get; set; }
+        public double Amount { get; set; }
     }
 }
